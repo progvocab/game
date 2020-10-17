ed0a1056e93787ef3b0237508ec5eca5d2732b7b
prog vocab <shahbazsaz07@gmail.com>
2020-10-17 22:46:03 +0530

    changes

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 var currentStep = 0;
 var margin = -100;
-var marginTop = screen.height-150;
+var marginTop = screen.height-350;
 var loc =  [" -0px 0px"," -176px 0px"," -352px 0px"," -511px 0px"," -700px 0px"," -0px -180px"," -176px -180px"," -352px -180px"," -528px -180px"," -704px -180px"," -0px -360px"," -166px -360px"," -342px -360px"," -511px -360px"] ;
 var width = ["169px","169px","169px","179px","179px",    "169px","169px","169px","169px","169px",      "165px","162px","170px" , "190px"];
 setInterval(function () {
@@ -16,7 +16,7 @@ setInterval(function () {
     
     if(margin > screen.width || marginTop <-170){
         margin = -100;
-        marginTop =screen.height-150;
+        marginTop =screen.height-350;
     } 
     bird.css('margin-left',margin );
     bird.css('margin-top',marginTop );
